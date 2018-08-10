cb3740542af266ef1725521f0684e7eefa5348eb
Marcus Schwarz <schwarz@sc-networks.com>
2018-08-10 15:02:30 +0200

    fixes #58 and #59

--- src/PHPTAL.php
+++ src/PHPTAL.php
@@ -237,8 +237,8 @@ class PHPTAL implements PhpTalInterface
         $this->codeFile = null;
         $this->path = $path;
         $this->source = null;
-        $this->context->docType = null;
-        $this->context->xmlDeclaration = null;
+        $this->context->_docType = null;
+        $this->context->_xmlDeclaration = null;
         return $this;
     }
 
@@ -260,8 +260,8 @@ class PHPTAL implements PhpTalInterface
         $this->codeFile = null;
         $this->source = new StringSource($src, $path);
         $this->path = $this->source->getRealPath();
-        $this->context->docType = null;
-        $this->context->xmlDeclaration = null;
+        $this->context->_docType = null;
+        $this->context->_xmlDeclaration = null;
         return $this;
     }
 
@@ -681,13 +681,13 @@ class PHPTAL implements PhpTalInterface
             }
 
             // unshift doctype
-            if ($this->context->docType) {
-                $res = $this->context->docType . $res;
+            if ($this->context->_docType) {
+                $res = $this->context->_docType . $res;
             }
 
             // unshift xml declaration
-            if ($this->context->xmlDeclaration) {
-                $res = $this->context->xmlDeclaration . "\n" . $res;
+            if ($this->context->_xmlDeclaration) {
+                $res = $this->context->_xmlDeclaration . "\n" . $res;
             }
 
             if ($this->postfilter !== null) {
